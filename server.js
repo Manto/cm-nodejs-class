855ca35f401bd84c30997b6ed4d530605bfcc332
Shawn Chiao <cm.shawn.chiao@gmail.com>
2016-04-07 23:45:33 +0000

    added simple poll object and poll page, with links from home page

--- server.js
+++ server.js
@@ -6,11 +6,42 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'base'}));
 app.set('view engine', 'handlebars');
 
+POLLS = [
+   {
+      id: 1,
+      name: "Will Warriors Win NBA Championship?"
+   },
+   {
+      id: 2,
+      name: "Will Donald Trump Win Republican Nomination?"
+   },
+];
+
 app.get('/', function(req, res) {
    res.render('home', {
-       now : new Date()
+       now : new Date(),
+       polls: POLLS
    });
 });
+
+function findPoll(poll_id) {
+   for(var i=0; i < POLLS.length; i++) {
+      var poll = POLLS[i];
+      if (poll.id == poll_id) {
+         return poll;
+      }
+   }
+   
+   return null;
+}
+
+app.get('/polls/:poll_id', function(req, res) {
+   var poll_id = req.params.poll_id;
+   
+   var poll = findPoll(poll_id);
+   res.render('poll', poll);
+});
+
 
 app.get('/about', function(req, res) {
    res.render('about');
