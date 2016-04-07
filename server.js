0be4c4826379d41cd86fe86d7fdbdda2968a92fb
Shawn Chiao <cm.shawn.chiao@gmail.com>
2016-04-07 22:47:31 +0000

    added routes and handlebars templates

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require("express");
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-   res.send('Hello!');
+   res.render('home', {
+       now : new Date()
+   });
 });
+
+app.get('/about', function(req, res) {
+   res.render('about');
+});
+
+app.use('/static', express.static('public'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
