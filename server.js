924a841346f084758fbb1ec2488d5454cf8cc49f
Shawn Chiao <shawn.chiao@elationemr.com>
2016-04-13 23:51:56 -0700

    access poll data from MongoDB
    removed unnecessary helper
    
    This reverts commit 5079538adaeed92658aa55b673341040d230332e.
    
    access poll data from MongoDB
    
    removed unnecessary helper
    
    This reverts commit 5079538adaeed92658aa55b673341040d230332e.

--- server.js
+++ server.js
@@ -1,45 +1,37 @@
+// Initialize express and handlebars packages
 var express = require("express");
 var exphbs  = require('express-handlebars');
+
+// Initialize the packages we need to access MongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodejs');
 
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'base'}));
 app.set('view engine', 'handlebars');
 
-POLLS = [
-   {
-      id: 1,
-      name: "Will Warriors Win NBA Championship?"
-   },
-   {
-      id: 2,
-      name: "Will Donald Trump Win Republican Nomination?"
-   },
-];
-
+// Defining routes
 app.get('/', function(req, res) {
-   res.render('home', {
-       now : new Date(),
-       polls: POLLS
+   var collection = db.get('polls');
+
+   // Find all polls from MongoDB and pass that data over to template
+   collection.find({},{},function(error,data){
+      res.render('home', {
+          now : new Date(),
+          polls: data
+      });
    });
 });
 
-function findPoll(poll_id) {
-   for(var i=0; i < POLLS.length; i++) {
-      var poll = POLLS[i];
-      if (poll.id == poll_id) {
-         return poll;
-      }
-   }
-   
-   return null;
-}
-
 app.get('/polls/:poll_id', function(req, res) {
+   var collection = db.get('polls');
    var poll_id = req.params.poll_id;
    
-   var poll = findPoll(poll_id);
-   res.render('poll', poll);
+   collection.findById(poll_id,function(error,poll){
+      res.render('poll', poll);
+   });
 });
 
 
@@ -47,7 +39,9 @@ app.get('/about', function(req, res) {
    res.render('about');
 });
 
+// Define a static folder to serve images, CSS files, etc
 app.use('/static', express.static('public'));
 
+// Start the server!
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
